a23c33c601d56a43fd49a848b0ce112c8ca5c7bf
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-05-22 01:46:53 -0700

    set the folder structure of backend and created custom error middleware and custom error function

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,6 +17,21 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.listen(process.env.port, () => {
+  console.log(`Server is running on port ${process.env.port}`);
+});
+
+// custom error middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
