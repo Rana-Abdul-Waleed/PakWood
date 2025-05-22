423fff1a6a9b401c5157a046a215adfcea874149
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-05-22 02:20:29 -0700

    created user model

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
+// database connectivity
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -16,12 +17,14 @@ mongoose
     console.log(err);
   });
 
+// express app
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+// run the server
 app.listen(process.env.port, () => {
   console.log(`Server is running on port ${process.env.port}`);
 });
---
+++ backend/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
